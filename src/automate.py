0f97e327ab1e4d25fca402edaeab296a887a38d4
Vincent Farcette <vincent.farcette@target2sell.com>
2016-12-15 12:17:28 +0100

    Documentation

--- src/automate.py
+++ src/automate.py
@@ -2,11 +2,29 @@ __author__ = 'vincent farcette'
 
 
 class AutomateMeta(type):
+    """
+    Metaclass used to create a linear cellular automate class able to compute the next state of a cell.
+    When used, rule_number must be defined as a keyword argument.
+    ex :
+
+    class Automate30(object, metaclass=AutomateMeta, rule_number=30):
+        pass
+
+    or
+
+    Automate30 = AutomateMeta('Automate30', (object,) , {'metaclass": AutomateMeta, ...}, rule_number=rule_number)
+    """
 
     # It is absolutely necessary to declare rule_number as a kwarg.
     def __new__(mcs, name, bases, namespace, *, rule_number=None):
 
         def next_step(self, initial_conditions):
+            """
+            Compute the state of the future cell.
+            :param self:
+            :param initial_conditions: three bytes int representing state of three cells predecessor.
+            :return: 1 or 0 depending on initial_conditions and self._rule_number.
+            """
             if not isinstance(initial_conditions, int) or not 0 <= initial_conditions < 8:
                 raise ValueError('Initial conditions are represented by a int 0 <= initial_conditions < 8')
             return 1 if 1 << initial_conditions & self._rule_number else 0
@@ -21,6 +39,17 @@ class AutomateMeta(type):
 
 
 class AutomateCache(object):
+
+    """
+    Class using AutomateMeta to create and cache concrete AutomateN classes.
+    Usage :
+    cache = AutomateCache()
+    rule_number = 12
+    automate12_instance = cache.get(rule_number)    # If this is the first call of cache.get(12), class is cached.
+                                                    # Anyway, an instance of Automate12 is returned.
+
+    AutomateN instance has an next_step(initial_conditions) method.
+    """
 
     __CACHED_AUTOMATES = {}
 
